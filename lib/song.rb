9376741695731bb1e1f711dc4abc3ecc73f2670a
jontenthoff <jontenthoff@hotmail.com>
2018-09-19 16:17:33 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,7 +10,7 @@ class Song
     self.class.all << self
   end
 
-  def self.create
+  def self.create(name)
     @@all << self.new 
   end
   
@@ -18,5 +18,5 @@ class Song
     
   end
   
-  def self.create_by_name 
+  def self.create_by_name(name) 
 end
